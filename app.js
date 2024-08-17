e09c15028806bfb89de6ba36a9903eabeea6ef2b
baargava ram <111219460+baargava@users.noreply.github.com>
2024-08-17 12:36:39 +0530

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { productService, productRoutes } = require('./Routes/ProductService')
+const productService = require('./Routes/ProductService')
 const categoryService = require('./Routes/CategoryService')
 const userService = require('./Routes/UserService')
 // const bodyParser = require('body-parser')
@@ -17,20 +17,12 @@ app.use(express.json()); // it is a middleware , used to show json data in app..
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(productService)
-app.use(categoryService)
+app.use('/products', productService)
+app.use('/category', categoryService)
 app.use(userService)
 
 app.get('/', (req, res) => {
-    const endpoints = {
-        products: productRoutes,
-        categories: "/category",
-        users: "/users"
-    };
-    res.json({
-        message: "Welcome to our API. Here are the available endpoints:",
-        endpoints
-    });
-});
+    res.send("hello world")
+})
 
 app.listen(port, () => console.log('server running on port'))   // server port,
